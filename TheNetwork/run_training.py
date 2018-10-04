3b7c60491f22fa81b21f73aabf9cd80f9fbac080
Dalya Gartzman <dalyag@gmail.com>
2018-10-05 01:28:03 +0300

    VeGGie is ready :) starting to train...

---
+++ TheNetwork/run_training.py
@@ -0,0 +1,31 @@
+
+from optparse import OptionParser
+
+from TheNetwork.whisper_detector import WhisperDetector
+
+
+def Main():
+    parser = OptionParser()
+    parser.add_option("-m", "--max_num_pics_per_category",
+                      help="For testing purposes, specify upper limit for images to load. "
+                           "This value should be larger than the batch size.")
+    parser.add_option("-e", "--epochs",
+                      help="Number of times to train on the whole dataset.")
+    parser.add_option("-b", "--batch_size",
+                      help="Size of one batch of training. keep small for the large VeGGie network.")
+    parser.add_option("-w", "--weights_filename",
+                      help="When, for fail-safe reasons, training in separate batches, load weights of previous train.")
+    options, _ = parser.parse_args()
+
+    m = int(options.max_num_pics_per_category)
+    e = int(options.epochs)
+    b = int(options.batch_size)
+    whisper_detector = WhisperDetector(m, e, b)
+
+    whisper_detector.build()
+    if options.weights_filename is not None:
+        whisper_detector.load_weights(options.weights_filename)
+    whisper_detector.train()
+
+if __name__ == '__main__':
+    Main()
--- TheNetwork/whisper_detector.py
+++ TheNetwork/whisper_detector.py
@@ -1,14 +1,9 @@
 
 from __future__ import print_function
 import keras
-from keras.datasets import cifar100
 from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
 from keras import optimizers
 import numpy as np
-from keras import regularizers
 import os
 import json
 
@@ -16,32 +11,42 @@ from TheNetwork.veggie import VeGGieModel
 
 class WhisperDetector(object):
     """"""
-    def __init__(self, path):
+    def __init__(self, max_num_pics_per_category=None, epochs=250, batch_size=24):
+        self.max_num_pics_per_category = max_num_pics_per_category or float('inf')
+        self.epochs = epochs
+        self.batch_size = batch_size
         self.model = None
-        self.data_path = path
 
     def build(self):
         """Build the VeGGie architecture."""
         veggie_model = VeGGieModel()
         self.model = veggie_model.build_veggie_model()
 
+    def load_weights(self, h5_filename):
+        """For fail-safe reasons, sometimes we train in separate epochs, and save weights between epochs."""
+        print("Loading weights of previously trained model.")
+        self.model.load_weights(h5_filename)
+
     def json_filename_to_array(self, json_filename):
-       a = json.load(open(json_filename))
-       a = np.array([[[pix for pix in row] for row in color] for color in a])
-       a = a.transpose(1, 2, 0)
-       return a
+        """Load .json filename into a numpy array that fits into VeGGie network."""
+        a = json.load(open(json_filename))
+        a = np.array([[[pix for pix in row] for row in color] for color in a])
+        a = a.transpose(1, 2, 0)
+        return a
 
     def folder_to_array(self, folder_path):
+        """Load all images from a folder and put in a numpy array of one batch."""
         array_list = []
         for i, filename in enumerate(os.listdir(folder_path)):
             arr = self.json_filename_to_array(folder_path + "/" + filename)
             array_list.append(arr)
-            if i > 150:
+            if i > self.max_num_pics_per_category:
                 break
         res = np.asarray(array_list)
         return res
 
     def unison_shuffled_copies(self, a, b):
+        """Shuffle order of input photos in the batch."""
         assert len(a) == len(b)
         p = np.random.permutation(len(a))
         return a[p], b[p]
@@ -50,10 +55,10 @@ class WhisperDetector(object):
         """
         Loads the data, split between train and test sets and shuffles it
         """
-        train_path_stegged = r"C:\Users\Asya\Code\dataHack\data\train\stegged"
-        train_path_not_stegged = r"C:\Users\Asya\Code\dataHack\data\train\not_stegged"
-        test_path_stegged = r"C:\Users\Asya\Code\dataHack\data\validate\stegged"
-        test_path_not_stegged = r"C:\Users\Asya\Code\dataHack\data\validate\not_stegged"
+        train_path_stegged = 'C:/Users/Rey/Projects/TheImageWhisperer/Data/train/stegged'
+        train_path_not_stegged = 'C:/Users/Rey/Projects/TheImageWhisperer/Data/train/not_stegged'
+        test_path_stegged = 'C:/Users/Rey/Projects/TheImageWhisperer/Data/validate/stegged'
+        test_path_not_stegged = 'C:/Users/Rey/Projects/TheImageWhisperer/Data/validate/not_stegged'
 
         x_train_stegged = self.folder_to_array(train_path_stegged)
         x_train_not_stegged = self.folder_to_array(train_path_not_stegged)
@@ -76,10 +81,12 @@ class WhisperDetector(object):
         return (x_train, y_train), (x_test, y_test)
 
     def normalize(self, X_train, X_test):
-        # this function normalize inputs for zero mean and unit variance
-        # it is used when training a model.
-        # Input: training set and test set
-        # Output: normalized training set and test set according to the trianing set statistics.
+        """
+        this function normalize inputs for zero mean and unit variance
+        it is used when training a model.
+        Input: training set and test set
+        Output: normalized training set and test set according to the trianing set statistics.
+        """
         mean = np.mean(X_train, axis=(0, 1, 2, 3))
         std = np.std(X_train, axis=(0, 1, 2, 3))
         X_train = (X_train - mean) / (std + 1e-7)
@@ -96,20 +103,15 @@ class WhisperDetector(object):
         of CIFAR10 images that a random half of them were manipulated using various steganography algorithms.
         """
         # training parameters
-        batch_size = 128
-        maxepoches = 250
+        batch_size = self.batch_size
+        maxepoches = self.epochs
         learning_rate = 0.1
         lr_decay = 1e-6
         lr_drop = 20
 
         (x_train, y_train), (x_test, y_test) = self.load_data()
 
-        def lr_scheduler(epoch):
-             return learning_rate * (0.5 ** (epoch // lr_drop))
-
-        reduce_lr = keras.callbacks.LearningRateScheduler(lr_scheduler)
-
-        #data augmentation - only flip as we don't want to harm the stegged data
+        # data augmentation - only flip as we don't want to harm the stegged data
         datagen = ImageDataGenerator(
             horizontal_flip=True,  # randomly flip images
             vertical_flip=True)  # randomly flip images
@@ -117,23 +119,29 @@ class WhisperDetector(object):
 
         # optimization details
         sgd = optimizers.SGD(lr=learning_rate, decay=lr_decay, momentum=0.9, nesterov=True)
-        # self.model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
         self.model.compile(loss='mean_squared_error', optimizer=sgd, metrics=['accuracy'])
 
         # training process in a for loop with learning rate drop every 25 epoches.
+        reduce_lr = self.reduce_lr(learning_rate, lr_drop)
+        self.model.fit_generator(datagen.flow(x_train, y_train,
+                                              batch_size=batch_size),
+                                 steps_per_epoch=x_train.shape[0] // batch_size,
+                                 epochs=maxepoches,
+                                 validation_data=(x_test, y_test), callbacks=[reduce_lr], verbose=2)
 
-        historyvtemp = self.model.fit_generator(datagen.flow(x_train, y_train,
-                                                             batch_size=batch_size),
-                                               steps_per_epoch=x_train.shape[0] // batch_size,
-                                               epochs=maxepoches,
-                                               validation_data=(x_test, y_test), callbacks=[reduce_lr], verbose=2)
-        self.model.fit_generator(
-            train_generator,
-            steps_per_epoch=2000 // batch_size,
-            epochs=50,
-            validation_data=validate_generator,
-            validation_steps=800 // batch_size)
-        self.model.save_weights('veggie.h5')
+        # self.model.fit_generator(datagen.flow(x_train, y_train,
+        #                                       batch_size=batch_size),
+        #                          steps_per_epoch=x_train.shape[0] // batch_size,
+        #                          epochs=maxepoches,
+        #                          validation_data=(x_test, y_test), verbose=2)
+
+
+        self.save_trained_model('veggie.h5')
+
+    def reduce_lr(self, learning_rate, lr_drop):
+        """Keras callback to reduce learning rate as the learning progresses."""
+        return keras.callbacks.LearningRateScheduler(
+            lambda epoch: learning_rate * (0.5 ** (epoch // lr_drop)))
 
     def predict(self, json_file):
         arr = self.json_filename_to_array(json_file)
