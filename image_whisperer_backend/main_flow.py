dd7457b7694508d6f67503d5bdbf62c003c9d2fe
Maayan Lahav <maayan.lahav@ridewithvia.com>
2018-10-05 03:08:49 +0300

    remove dependency in mxnet

--- image_whisperer_backend/main_flow.py
+++ image_whisperer_backend/main_flow.py
@@ -8,7 +8,7 @@ flow:
 import os
 import json
 import cv2
-import mxnet as mx
+# import mxnet as mx
 import numpy as np
 import sys
 import logging
@@ -33,8 +33,8 @@ def main_flow(image_file_name):
     logging.info("main flow, and this is the image file name - {!s}".format(image_file_name))
     path_to_json_image_file = _get_path_to_image_as_json(image_file_name)
     result = _check_if_image_was_encrypted(path_to_json_image_file)
-    bmp_path = _transform_array_to_bmp_file(path_to_json_image_file)
-    _present_result(result, bmp_path)
+    # bmp_path = _transform_array_to_bmp_file(path_to_json_image_file)
+    _present_result(result)
 
 
 def _get_path_to_image_as_json(image_file_name):
@@ -44,7 +44,7 @@ def _get_path_to_image_as_json(image_file_name):
     image_represented_as_lists = _cifar_image_to_array(chosen_image_path)
     json_path = os.path.join(dir_of_current_file, "..", "images", image_file_name.split(".")[0] + ".json")
     with open(json_path, "w") as f:
-        json.dump(image_represented_as_lists.asnumpy().tolist(), f)
+        json.dump(image_represented_as_lists.tolist(), f)
     logging.info("this is the json path in _get_path_to_image_as_json - {!s}".format(json_path))
     return json_path
 
@@ -70,28 +70,28 @@ def _json_filename_to_array(json_filename):
 def _cifar_image_to_array(path):
     logging.info("run this function - _cifar_image_to_array, and this is the path - {!s}".format(path))
     srcBGR = cv2.imread(path)
-    dest = cv2.cvtColor(srcBGR, cv2.COLOR_BGR2RGB)
-    array = dest.transpose(2, 0, 1)
-    mx_ex_int_array = mx.nd.array(array)
+    mx_ex_int_array = cv2.cvtColor(srcBGR, cv2.COLOR_BGR2RGB)
+    # array = dest.transpose(2, 0, 1)
+    # mx_ex_int_array = mx.nd.array(array)
     return mx_ex_int_array
 
 
-def _transform_array_to_bmp_file(array_path):
-    logging.info("_transform_array_to_bmp_file, and this is the array path - {!s}".format(array_path))
-    array = _json_filename_to_array(array_path)
+# def _transform_array_to_bmp_file(array_path):
+#     logging.info("_transform_array_to_bmp_file, and this is the array path - {!s}".format(array_path))
+#     array = _json_filename_to_array(array_path)
+#
+#     file_name = os.path.splitext(array_path)[0]
+#     bmp_path = file_name + ".bmp"
+#
+#     cv2.imwrite(bmp_path, array)
+#     logging.info("this is the bmp_path - {!s}".format(bmp_path))
+#     return bmp_path
 
-    file_name = os.path.splitext(array_path)[0]
-    bmp_path = file_name + ".bmp"
 
-    cv2.imwrite(bmp_path, array)
-    logging.info("this is the bmp_path - {!s}".format(bmp_path))
-    return bmp_path
-
-
-def _present_result(result, image_path):
-    logging.info("_present result, this is the image_path - {!s}".format(image_path))
+def _present_result(result):
+    # logging.info("_present result, this is the image_path - {!s}".format(image_path))
     # the STDOUT is reported to the front-end.
-    print 1
+    print result
 
 
 if __name__ == "__main__":
